ec48c30558f62074f2b6c9f3b19a0442971bca44
Lumir Mrkva <lumir@mrq.cz>
2014-08-01 01:57:18 +0200

    file servlet

---
+++ vocloud/src/main/java/cz/mrq/vocloud/servlet/FileServlet.java
@@ -0,0 +1,109 @@
+package cz.mrq.vocloud.servlet;
+
+import cz.mrq.vocloud.ejb.JobFacade;
+import cz.mrq.vocloud.entity.Job;
+import cz.mrq.vocloud.tools.Config;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLDecoder;
+
+/**
+ * FileServlet
+ *
+ * @author Lumir Mrkva (lumir.mrkva@topmonks.com)
+ */
+@WebServlet(name = "ImagesServlet", urlPatterns = {"/jobs/preview/*"})
+public class FileServlet extends HttpServlet {
+
+    @EJB
+    JobFacade jf;
+
+    @Inject
+    @Config
+    private String jobsDir;
+
+    private static final int DEFAULT_BUFFER_SIZE = 10240;
+
+    public void init() throws ServletException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = request.getPathInfo();
+
+        if (path == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
+            return;
+        }
+
+        String[] split = path.split("/");
+        String jobId = split[1];
+
+        Job job = jf.find(Long.parseLong(jobId));
+        String user = request.getRemoteUser();
+        if (!job.getOwner().getUsername().equals(user)) {
+            response.sendError(403);
+            return;
+        }
+
+        File file = new File(jobsDir, URLDecoder.decode(path, "UTF-8"));
+
+        if (!file.exists()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
+            return;
+        }
+
+        String contentType = getServletContext().getMimeType(file.getName());
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        // Init servlet response.
+        response.reset();
+        response.setBufferSize(DEFAULT_BUFFER_SIZE);
+        response.setContentType(contentType);
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+
+        // Prepare streams.
+        BufferedInputStream input = null;
+        BufferedOutputStream output = null;
+
+        try {
+            // Open streams.
+            input = new BufferedInputStream(new FileInputStream(file), DEFAULT_BUFFER_SIZE);
+            output = new BufferedOutputStream(response.getOutputStream(), DEFAULT_BUFFER_SIZE);
+
+            // Write file contents to response.
+            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+            int length;
+            while ((length = input.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+        } finally {
+            // Gently close streams.
+            close(output);
+            close(input);
+        }
+    }
+
+    private static void close(Closeable resource) {
+        if (resource != null) {
+            try {
+                resource.close();
+            } catch (IOException e) {
+                // Do your thing with the exception. Print it, log it or mail it.
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
