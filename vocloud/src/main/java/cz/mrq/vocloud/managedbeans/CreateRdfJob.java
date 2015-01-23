a4a5de344f1d356833d64fbd7e6a560c34f6c5e7
Andrej Palička <palicka@netlife.sk>
2015-01-23 10:28:45 +0100

    rdf worker should be working now

---
+++ vocloud/src/main/java/cz/mrq/vocloud/managedbeans/CreateRdfJob.java
@@ -0,0 +1,101 @@
+package cz.mrq.vocloud.managedbeans;
+
+import cz.mrq.vocloud.helper.EditPanel;
+import org.apache.commons.io.FileUtils;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import org.zeroturnaround.zip.ZipUtil;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CreateRdfJob
+ *
+ * @author Andrej Palicka (palicka@netlife.sk)
+ */
+@ManagedBean(name = "createRdfJob")
+@ViewScoped
+
+public class CreateRdfJob extends CreateJob {
+
+    File parameters;
+
+    Boolean zip = false;
+    Boolean config = false;
+    EditPanel editPanel = new EditPanel();
+
+    static String CONFIG_FILE = "config.json";
+
+    @Override
+    public String getJobType() {
+        return "RDF";
+    }
+
+    @Override
+    public void postInit() {
+        List<File> files = new ArrayList<>();
+        editPanel.getPanel().setCollapsed(true);
+        for (File file : uploadedFiles) {
+            if (file.getName().endsWith(".zip") && !file.getName().equals("results.zip")) {
+                if (zip = ZipUtil.containsEntry(file, CONFIG_FILE)) {
+                    parameters = file;
+                    editPanel.setFileContents(new String(ZipUtil.unpackEntry(file, CONFIG_FILE)));
+                    files.add(file);
+                }
+            }
+        }
+        uploadedFiles = files;
+    }
+
+    @Override
+    protected File prepareParameters() {
+        File params = new File(getJobFolder(), parameters.getName());
+        String config = editPanel.getFileContents();
+        try {
+            FileUtils.writeStringToFile(new File(getJobFolder(), CONFIG_FILE), config);
+        } catch (IOException e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "failed to save config.json to job directory", null));
+        }
+        ZipUtil.replaceEntry(params, CONFIG_FILE, config.getBytes());
+        return params;
+    }
+
+    @Override
+    public void handleFileUpload(FileUploadEvent event) {
+        UploadedFile uploaded = event.getFile();
+        if (uploaded.getFileName().endsWith(".zip")) {
+            File file = new File(uploadDir, uploaded.getFileName());
+            copyUploadedFile(file, uploaded);
+            if (ZipUtil.containsEntry(file, CONFIG_FILE)) {
+                zip = true;
+                parameters = file;
+                uploadedFiles.add(parameters);
+                editPanel.setFileContents(new String(ZipUtil.unpackEntry(file, CONFIG_FILE)));
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                        "config.json not found in uploaded zip file", null));
+            }
+        }
+    }
+
+    @Override
+    public void handleSave() {
+
+    }
+
+    public Boolean getZip() {
+        return zip;
+    }
+
+    public EditPanel getEditPanel() {
+        return editPanel;
+    }
+}
--- worker/rdf/src/main/java/cz/mrq/vocloud/worker/rdf/Config.java
+++ worker/rdf/src/main/java/cz/mrq/vocloud/worker/rdf/Config.java
@@ -1,19 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cz.mrq.vocloud.worker.rf;
+package cz.mrq.vocloud.worker.rdf;
 
 /**
+ * Config
  *
- * @author palicka
+ * @author Lumir Mrkva (lumir@mrq.cz)
  */
 public class Config {
+
     public static String binariesLocation = "/app/rdf";
 
     public static String resultsDir;
     public static String resultsLink;
     public static String serverAddress = "http://localhost:8080";
     public static int maxJobs = 4;
+
+
 }
