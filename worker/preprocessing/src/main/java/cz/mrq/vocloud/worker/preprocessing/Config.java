41b0a275e935f5bda838cc52d3304c7cec410353
Andrej Palička <palicka@netlife.sk>
2015-03-22 19:46:40 +0100

    preprocessing worker (not working properly yet)

---
+++ worker/preprocessing/src/main/java/cz/mrq/vocloud/worker/preprocessing/Config.java
@@ -0,0 +1,18 @@
+package cz.mrq.vocloud.worker.preprocessing;
+
+/**
+ * Config
+ *
+ * @author Lumir Mrkva (lumir@mrq.cz)
+ */
+public class Config {
+
+    public static String binariesLocation = "/app/preprocess";
+
+    public static String resultsDir;
+    public static String resultsLink;
+    public static String serverAddress = "http://vocloud-dev:8001";
+    public static int maxJobs = 4;
+
+
+}
